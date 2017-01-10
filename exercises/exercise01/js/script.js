767751b53a431024dce8a2aad068f05fa3a965fa
rebeccamalagisi <becky_mal@hotmail.com>
2017-01-10 17:26:18 -0500

    changes to exercise01

--- exercises/exercise01/js/script.js
+++ exercises/exercise01/js/script.js
@@ -1,36 +1,22 @@
-/*
-DART450
-Week 01
-Colour Change
-Just change the colour of the text to red if it is clicked using jQuery's
-.css() function
-*/
-
-// We use "document ready" so that our code only runs once the whole
-// webpage is loaded and ready.
-
 $(document).ready(function() {
 
   // We use the jQuery .click() function to "listen" for clicks on our
   // div with id "text"
 
-  $('#text').click(function () {
-
-    // This is what we do when it is clicked
+  $('.newcolor').hover(function () {
+    // This is what we do when this class if hovered
 
     // Inside the function "this" refers to the thing that got clicked
-    // And we use .css() to set its CSS color property to ""
-    $(this).css('color','red');
+    // And we use .toggleClass() to animate by applying a CSS class with
+    // animation properties
+    $(this).addClass('colorme');
+  },function () {
 
-    // What would happen if we didn't use "this" and used "#text" instead?
-
-    // What if there was more than one "#text" id in the HTML?
+    // Note that .hover() takes TWO functions!
+    // The first one is for what to do when the hover STARTS
+    // This second one is for what to do when the hover ENDS
+    // (We don't want to do anything in our case)
 
   });
-
-
-
-
-
 
 });
