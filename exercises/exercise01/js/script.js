847b42f786a45b303ecaa3165e3dd5ec070b3c23
rebeccamalagisi <becky_mal@hotmail.com>
2017-01-16 13:00:44 -0500

    updated css and js

--- exercises/exercise01/js/script.js
+++ exercises/exercise01/js/script.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
 
-  // We use the jQuery .click() function to "listen" for clicks on our
-  // div with id "text"
+  
 
-  $('.newcolor').hover(function () {
-    // This is what we do when this class if hovered
+  $('.newcolor1').hover(function () {
 
-    // Inside the function "this" refers to the thing that got clicked
-    // And we use .toggleClass() to animate by applying a CSS class with
-    // animation properties
-    $(this).addClass('colorme');
-  },function () {
+      $(this).addClass('colorme1');
 
-    // Note that .hover() takes TWO functions!
-    // The first one is for what to do when the hover STARTS
-    // This second one is for what to do when the hover ENDS
-    // (We don't want to do anything in our case)
+    },function () {
 
   });
+
+
+  $('.newcolor2').hover(function () {
+
+      $(this).addClass('colorme2');
+
+    },function () {
+
+  });
+
 
 });
