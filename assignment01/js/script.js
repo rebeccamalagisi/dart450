6a04c23d556f033ea424a4aecb854f9e951b3063
rebeccamalagisi <becky_mal@hotmail.com>
2017-02-19 17:05:26 -0500

    testing some things

--- assignment01/js/script.js
+++ assignment01/js/script.js
@@ -1,14 +1,25 @@
 /*
 
-Title of project
-Author's name
+Assignment 01 - Interactive Personality
+Rebecca Malagisi
 
+An interactive click based website with random elements in order to waste your time.
 Description of this script in the context of the project
 
 */
 
 $(document).ready(function() {
 
+
+  $("#one").click(function(){
+    $(this).animate({'left': '+=200px'}, 200);
+
+  
+
+  });
   // Write code in here that will run when the document is loaded
 
 });
+
+
+//function
