cf6346b0a4bf746284795b7816a27104dc12bf6d
rebeccamalagisi <becky_mal@hotmail.com>
2017-02-20 01:14:23 -0500

    added more speech intervals + changed shapes into rectangles

--- assignment01/js/script.js
+++ assignment01/js/script.js
@@ -3,8 +3,11 @@
 Assignment 01 - Interactive Personality
 Rebecca Malagisi
 
-An interactive click based website with random elements in order to waste your time.
-Every minute you will get an update starting how much time you've wasted.
+An interactive website meant to waste your time.
+Click on the squares to clear them, but it may be harder than it seems.
+(don't forget to scroll!!!)
+After the first minute you will get an update stating how much time you've wasted.
+Afterwards, you will receive a random message from an array of choices every 20 seconds.
 
 */
 
@@ -30,14 +33,19 @@ var answers = [
   "You're wasting time.",
   "Get back to work.",
   "Stop being lazy.",
-  // "Shouldn't you be doing something more productive with your time?",
+  "You'll regret procrastinating later."
   // "Shouldn't you be doing something more productive with your time?",
   // "Shouldn't you be doing something more productive with your time?",
 
 ];
 
-const NUM_RANDOM_SQUARES = 6;
-// const INTERVAL = randomIntegerInRange(1000,5000);
+
+
+
+// For the square divs for loop
+const NUM_RANDOM_RECTS = 6;
+const INTERVAL = 2500;
+
 
 
 
@@ -59,23 +67,43 @@ $(document).ready(function() {
       counter++;
 
       // Speech element to let you know when a minute has passed
-      if (counter == 10) {
+      if (counter == 60) {
         sayOne("You just wasted a minute of your life.");
         console.log("speech is okay");
 
       }
 
-      else if (counter > 10) {
-
-          console.log("right on, pal");
-
-
+      else if (counter == 75) {
+        sayTwo(answers);
+        console.log("right on, pal");
       }
+
+      else if (counter == 90) {
+        sayTwo(answers);
+        console.log("right on, pal");
+      }
+
+      else if (counter == 105) {
+        sayTwo(answers);
+        console.log("right on, pal");
+      }
+
+      else if (counter == 120) {
+        sayTwo(answers);
+        console.log("right on, pal");
+      }
+
+      else if (counter == 135) {
+        sayTwo(answers);
+        console.log("right on, pal");
+      }
+
+
   },timeInterval);
 
   // CIRCLE INTERVAL
   setInterval(function () {
-    for (var i = 0; i < NUM_RANDOM_SQUARES; i++) {
+    for (var i = 0; i < NUM_RANDOM_RECTS; i++) {
 
       // Will give a random width position
       var x = Math.random() * $(document).width();
@@ -84,14 +112,14 @@ $(document).ready(function() {
       var y = Math.random() * $(document).height();
 
 
-      var square = generateSquare(x,y);
+      var rect = generateRect(x,y);
 
       // Add click to circle
 
       // When mouse touches the squares, they will start to move down the page or 'run away' from the user
       $('div').mouseover(function(){
         $(this).animate({
-          'top': '+=80px'
+          'top': '+=76px'
 
         }, 100);
 
@@ -110,13 +138,13 @@ $(document).ready(function() {
 
 
       // And then add it to the page
-      $('body').append(square);
+      $('body').append(rect);
 
 
 
     }
 
-  },randomIntegerInRange(1000,5000));
+  }, INTERVAL);
 
 
 
@@ -128,10 +156,6 @@ $(document).ready(function() {
 
 ///////////////////////////////////////////////////////////////////////////
 // FUNCTIONS
-
-
-
-
 
 
 
@@ -171,18 +195,20 @@ function getRandomString(array) {
 
 
 
-function generateSquare(x, y) {
+function generateRect(x, y) {
 
-  var squareDiv = $('<div></div>');
+  var rectDiv = $('<div></div>');
+  var w = randomIntegerInRange(60,500);
+  var h = randomIntegerInRange(60,500);
   var randomRedValue = randomIntegerInRange(0,255);
   var randomBlueValue = randomIntegerInRange(0,255);
   var randomGreenValue = randomIntegerInRange(0,255);
 
-  squareDiv.css({
+  rectDiv.css({
     position: 'absolute',
     zIndex: '0',
-    width: '260px',
-    height: '260px', 
+    width: w + 'px',
+    height: h + 'px', 
     top: y + 'px',
     left: x + 'px', 
     backgroundColor: 'rgba(' + randomRedValue + ',' + randomGreenValue + ',' + randomBlueValue + ',1)'
@@ -190,20 +216,24 @@ function generateSquare(x, y) {
 
   });
 
-  return squareDiv;
+  return rectDiv;
 
 }
 
+
+
+// This is just an equation essentially.
+//
+// Math.random() returns a number between 0 and 1
+// Math.floor() removes anything after the decimal point
+// (max - min) gives us the size of the possible range of values
+//
+// So a random amount of that range + min will give us a
+// random number between min and max (not including max)
+
 function randomIntegerInRange(min,max) {
 
-  // This is just an equation essentially.
-  //
-  // Math.random() returns a number between 0 and 1
-  // Math.floor() removes anything after the decimal point
-  // (max - min) gives us the size of the possible range of values
-  //
-  // So a random amount of that range + min will give us a
-  // random number between min and max (not including max)
+
 
   return Math.floor(Math.random() * (max - min)) + min;
 }
