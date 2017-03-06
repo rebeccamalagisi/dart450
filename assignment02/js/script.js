d1151d287002bf941767d7fe6c7601ff043cafc8
rebeccamalagisi <becky_mal@hotmail.com>
2017-03-05 19:37:28 -0500

    Created files for Assignment 02

---
+++ assignment02/js/script.js
@@ -0,0 +1,14 @@
+/*
+
+Assignment 02 -
+Rebecca Malagisi
+
+Description of this script in the context of the project
+
+*/
+
+$(document).ready(function() {
+
+  // Write code in here that will run when the document is loaded
+
+});
