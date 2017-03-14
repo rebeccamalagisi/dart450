9379eeba3155d8b3aa0fe52ecdd8fceafcb81462
rebeccamalagisi <becky_mal@hotmail.com>
2017-03-13 22:44:49 -0400

    exercise08 files created

---
+++ exercises/exercise08/js/script.js
@@ -0,0 +1,14 @@
+/*
+
+Title of project
+Author's name
+
+Description of this script in the context of the project
+
+*/
+
+$(document).ready(function() {
+
+  // Write code in here that will run when the document is loaded
+
+});
