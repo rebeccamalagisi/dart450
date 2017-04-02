682c1fe42fff9434361240ac81f6a2d090e9cd64
rebeccamalagisi <becky_mal@hotmail.com>
2017-04-02 19:36:41 -0400

    setting up the information box (care instructions)

--- assignment02/js/script.js
+++ assignment02/js/script.js
@@ -9,6 +9,15 @@ Description of this script in the context of the project
 
 $(document).ready(function() {
 
+  $("#menu").click(function(){
+    $("#info").show();
+  });
+
+  $(".close").click(function(){
+    $("#info").hide();
+
+  });
+
   // Write code in here that will run when the document is loaded
 
 });
