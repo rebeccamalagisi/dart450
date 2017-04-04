90d0c3947bca3530796ed54dca874666f433fd8d
rebeccamalagisi <becky_mal@hotmail.com>
2017-04-04 11:29:22 -0400

    made care instructions menu into its own function

--- assignment02/js/script.js
+++ assignment02/js/script.js
@@ -9,15 +9,32 @@ Description of this script in the context of the project
 
 $(document).ready(function() {
 
+  // Make care instructions functional by calling it when the document loads
+  careInstructions();
+
+
+
+});
+
+
+
+
+
+// CARE INSTRUCTIONS ENABLING
+// (pretty self explanatory menu thingy)
+
+function careInstructions() {
+
+  // when the icon is clicked, show the info box of information
   $("#menu").click(function(){
     $("#info").show();
   });
 
+
+  // click the x to close the info box
   $(".close").click(function(){
     $("#info").hide();
 
   });
 
-  // Write code in here that will run when the document is loaded
-
-});
+};
