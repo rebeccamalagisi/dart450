8dbbfcd5a0ff489d92f7b1fec170deb6096c9d51
rebeccamalagisi <becky_mal@hotmail.com>
2017-04-04 12:48:59 -0400

    awake/asleep function applying css to background so user can see visually when the site is responsive

--- assignment02/js/script.js
+++ assignment02/js/script.js
@@ -1,20 +1,38 @@
 /*
 
-Assignment 02 -
+Assignment 02 - Personification of the web/Interactive personality
 Rebecca Malagisi
 
 Description of this script in the context of the project
 
 */
 
+
+
+var date = new Date();
+
+
+///////////////////////////////////////////////////////////////////////////
+
+// DOCUMENT.READY = SHIT THAT IS READY WHEN THE PAGE LOADS
+
 $(document).ready(function() {
 
   // Make care instructions functional by calling it when the document loads
   careInstructions();
 
+  awakeAsleep();
+
 
 
 });
+
+
+
+
+
+
+
 
 
 
@@ -40,3 +58,28 @@ function careInstructions() {
 };
 
 ///////////////////////////////////////////////////////////////////////////
+
+function awakeAsleep() {
+
+  // getHours will retrieve the hour from the computer
+  // if the time is between 10am and 8pm, the site is AWAKE (white background)
+  if (date.getHours() >= 9 && date.getHours() <= 21){
+
+    $("body").css({
+      backgroundColor : "white"
+    });
+
+  }
+
+  // ELSE, if the time is not 10am-8pm, the site is ASLEEP (black background)
+  else {
+
+    $("body").css({
+      backgroundColor : "black"
+    });
+
+  }
+
+
+
+};
