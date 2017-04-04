9a35f56c66d8ee0517b231afbbe22c22bd0f377d
rebeccamalagisi <becky_mal@hotmail.com>
2017-04-04 14:02:49 -0400

    more js comments, adding annyang library to html doc

--- assignment02/js/script.js
+++ assignment02/js/script.js
@@ -7,9 +7,16 @@ Description of this script in the context of the project
 
 */
 
+///////////////////////////////////////////////////////////////////////////
 
+// VARIABLES
 
+// retrieve date and time data from computer and store in date variable
 var date = new Date();
+// getHours will retrieve the hour from the computer (0-23 hours)
+// date.getHours --> date is the variable above
+// call the var theHour to callback easier later
+var theHour = date.getHours();
 
 
 ///////////////////////////////////////////////////////////////////////////
@@ -59,24 +66,39 @@ function careInstructions() {
 
 ///////////////////////////////////////////////////////////////////////////
 
+// AWAKE/ASLEEP function
+
 function awakeAsleep() {
 
-  // getHours will retrieve the hour from the computer
-  // if the time is between 10am and 8pm, the site is AWAKE (white background)
-  if (date.getHours() >= 9 && date.getHours() <= 21){
+  // theHour is var at the beginning and includes date.getHours()
+  // getHours will retrieve the hour from the computer (0-23 hours)
+  // date.getHours --> date is the variable at the beginning of the doc to retrieve Date() data from the computer
 
+  // if the hour is between 10am and 8pm, the site is AWAKE (white background)
+  // >= means greater than or equal to --- && means and --- <= means less than or equal to
+  if (theHour >= 9 && theHour <= 19){
+
+    // white background
     $("body").css({
-      backgroundColor : "white"
+      backgroundColor : "cornsilk" // change to white after
     });
 
   }
 
+
+
+
   // ELSE, if the time is not 10am-8pm, the site is ASLEEP (black background)
   else {
 
+    // black background
     $("body").css({
       backgroundColor : "black"
     });
+
+    // how to make non responsive??
+
+
 
   }
 
