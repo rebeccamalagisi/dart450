0c4e32573c2501a7359c111e418ba766106bd7b6
rebeccamalagisi <becky_mal@hotmail.com>
2017-04-07 10:44:55 -0400

    having some issues with audio & video

--- assignment02/js/script.js
+++ assignment02/js/script.js
@@ -38,39 +38,39 @@ var theDay = date.getDate(); // maybe unnecessary???
 
 
 
-// const INTERVAL = 100;
-// // How fast the emoticon faces should fade in and out
-// const ANIMATION_DURATION = 1500;
-//
-// const MAX_TIME_SINCE_FACE = 100;
-// // Track how long it has been since the page has seen a face,
-// // start on a high value so it assumes it hasn't seen one for a long time
-// var timeSinceFace = 1000;
-//
-// var noStrings = [
-//   "Where are you?",
-//   "Where did you go?",
-//   "I miss you.",
-//   "I can't see you.",
-//   "Come back.",
-//   "I'm so lonely.",
-//   "Why do you keep leaving me?",
-//   "Don't be shy.",
-//   "I wish you'd show me your face.",
-//   "Please don't hide from me."
-// ];
-// var yesStrings = [
-//   "There you are.",
-//   "Welcome back.",
-//   "Hey there, friend.",
-//   "I'm glad you're back.",
-//   "I missed you, pal.",
-//   "Stay with me for a while.",
-//   "You look so serious.",
-//   "You have nice eyes.",
-//   "You're so intriguing to watch.",
-//   "I wish I knew more about you."
-// ];
+const INTERVAL = 100;
+// How fast the emoticon faces should fade in and out
+const ANIMATION_DURATION = 500;
+
+const MAX_TIME_SINCE_FACE = 100;
+// Track how long it has been since the page has seen a face,
+// start on a high value so it assumes it hasn't seen one for a long time
+var timeSinceFace = 100;
+
+var noStrings = [
+  "Where are you?",
+  "Where did you go?",
+  "I miss you.",
+  "I can't see you.",
+  "Come back.",
+  "I'm so lonely.",
+  "Why do you keep leaving me?",
+  "Don't be shy.",
+  "I wish you'd show me your face.",
+  "Please don't hide from me."
+];
+var yesStrings = [
+  "There you are.",
+  "Welcome back.",
+  "Hey there, friend.",
+  "I'm glad you're back.",
+  "I missed you, pal.",
+  "Stay with me for a while.",
+  "You look so serious.",
+  "You have nice eyes.",
+  "You're so intriguing to watch.",
+  "I wish I knew more about you."
+];
 
 
 
@@ -95,29 +95,31 @@ $(document).ready(function() {
 
 
 
-  // Audio stuff
+  // // Audio stuff
+  //   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+  //
+  //   if (navigator.getUserMedia) {     
+  //     // Note that this time we use {audio: true} to get the microphone,
+  //     // otherwise it's the same as getting video.
+  //     navigator.getUserMedia({audio: true}, handleAudio, audioError);
+  //
+  //     console.log("AUDIO WORKING");
+  //   }
+  //
+  //   // We're going to repeatedly check the current audio volume
+  //   // in order to update the visibilty of the page content,
+  //   // so we need an interval
+  //   setInterval(update,CHECK_INTERVAL);
+
+
+    // video implementation
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-    if (navigator.getUserMedia) {     
-      // Note that this time we use {audio: true} to get the microphone,
-      // otherwise it's the same as getting video.
-      navigator.getUserMedia({audio: true}, handleAudio, audioError);
+    if (navigator.getUserMedia) {      
+      navigator.getUserMedia({video: true}, handleVideo, videoError);
 
-      console.log("AUDIO WORKING");
+
     }
-
-    // We're going to repeatedly check the current audio volume
-    // in order to update the visibilty of the page content,
-    // so we need an interval
-    setInterval(update,CHECK_INTERVAL);
-
-
-    // // video implementation
-    // if (navigator.getUserMedia) {      
-    //   navigator.getUserMedia({video: true}, handleVideo, videoError);
-    //
-    //
-    // }
 
 
   //   // grab our canvas
@@ -446,210 +448,210 @@ function clearLocal(event) {
 ///////////////////////////////////////////////////////////////////////////
 
 
-
-
-// AUDIO VARIABLES
-
-// How often to check the current volume
-const CHECK_INTERVAL = 100;
-
-// An audiocontext is used to work with audio
-var audioContext;
-// We will create an audio meter and put it in here
-var meter;
-// A place to store the output stream of the microphone
-var microphone;
-
-
-
-
-// handleAudio (stream)
+// 
 //
-// Called when we have access to the microphone's audio stream
-
-
-
-
-function handleAudio (stream) {
-  // Create our AudioContext for working with audio...
-  audioContext = new AudioContext();
-
-  // Store the audio stream from the microphone in our microphone variable
-  microphone = audioContext.createMediaStreamSource(stream);
-
-  // Create an audio meter for checking the volume
-  meter = createAudioMeter(audioContext);
-
-  // Connect the meter and the microphone so the meter has access
-  // the microphone stream
-  microphone.connect(meter);
-}
-
-// audioError ()
+// // AUDIO VARIABLES
 //
-// If something goes wrong, panic!
-
-function audioError(e) {
-  // $("body").append(sadSquare(100,100));
-}
-
-// update ()
+// // How often to check the current volume
+// const CHECK_INTERVAL = 100;
 //
-// Called every CHECK_INTERVAL milliseconds.
-// Checks to make sure the meter exists, and then sets the opacity
-// of our content div to be relative to the current volume.
-function update () {
-  if (meter) {
-    // meter.volume gives us a number between 0 (silence) and 1 (loudest possible)
-    // If you look at the value of meter.volume, it's often very, very small
-    // for ambient noise, so we multiple by 10000 to make our webpage more
-    // sensitive to noise
-    //
-    // We subtract that value from 1 because we want the opacity to get LOWER
-    // when the volume gets HIGHER.
-    var newOpacity = meter.volume*10 - 1;
-    if (newOpacity < 0) {
-      newOpacity = 0;
-    }
-    // Could also use: var newOpacity = Math.max(0, 1 - meter.volume*10000)
-    // if we don't want the if statement
-
-    // Now set the opacity
-    $('#audio').css({
-      opacity: Math.max(0, newOpacity)
-    });
-
-    // TRY THIS: just set newOpacity to be meter.volume instead,
-    // what does this do? How does it change your experience of the page?
-  }
-
-}
+// // An audiocontext is used to work with audio
+// var audioContext;
+// // We will create an audio meter and put it in here
+// var meter;
+// // A place to store the output stream of the microphone
+// var microphone;
+//
+//
+//
+//
+// // handleAudio (stream)
+// //
+// // Called when we have access to the microphone's audio stream
+//
+//
+//
+//
+// function handleAudio (stream) {
+//   // Create our AudioContext for working with audio...
+//   audioContext = new AudioContext();
+//
+//   // Store the audio stream from the microphone in our microphone variable
+//   microphone = audioContext.createMediaStreamSource(stream);
+//
+//   // Create an audio meter for checking the volume
+//   meter = createAudioMeter(audioContext);
+//
+//   // Connect the meter and the microphone so the meter has access
+//   // the microphone stream
+//   microphone.connect(meter);
+// }
+//
+// // audioError ()
+// //
+// // If something goes wrong, panic!
+//
+// function audioError(e) {
+//   // $("body").append(sadSquare(100,100));
+// }
+//
+// // update ()
+// //
+// // Called every CHECK_INTERVAL milliseconds.
+// // Checks to make sure the meter exists, and then sets the opacity
+// // of our content div to be relative to the current volume.
+// function update () {
+//   if (meter) {
+//     // meter.volume gives us a number between 0 (silence) and 1 (loudest possible)
+//     // If you look at the value of meter.volume, it's often very, very small
+//     // for ambient noise, so we multiple by 10000 to make our webpage more
+//     // sensitive to noise
+//     //
+//     // We subtract that value from 1 because we want the opacity to get LOWER
+//     // when the volume gets HIGHER.
+//     var newOpacity = meter.volume*50 - 1;
+//     if (newOpacity < 0) {
+//       newOpacity = 0;
+//     }
+//     // Could also use: var newOpacity = Math.max(0, 1 - meter.volume*10000)
+//     // if we don't want the if statement
+//
+//     // Now set the opacity
+//     $('#audio').css({
+//       opacity: Math.max(0, newOpacity)
+//     });
+//
+//     // TRY THIS: just set newOpacity to be meter.volume instead,
+//     // what does this do? How does it change your experience of the page?
+//   }
+//
+// }
 
 
 ///////////////////////////////////////////////////////////////////////////
 
 
-// // handleVideo (stream)
-// //
-// // When getUserMedia gets hold of the user's webcam, it calls this function
-// // with the argument "stream" which is the stream of the webcam data
-// function handleVideo(stream) {
-//   // First get the URL of the stream
-//   var streamURL = window.URL.createObjectURL(stream);
-//   // Now hold of the element on the page that can contain the video
-//   // (the video element with id 'webcam') and set its src attribute
-//   // to the URL we created for the stream...
-//   $('#webcam').attr({
-//     src: streamURL
-//   });
-//   // And now that the webcam is available, we can start actually tracking
-//   // colors with our tracking.js code...
-//   startTracking();
+// handleVideo (stream)
 //
+// When getUserMedia gets hold of the user's webcam, it calls this function
+// with the argument "stream" which is the stream of the webcam data
+function handleVideo(stream) {
+  // First get the URL of the stream
+  var streamURL = window.URL.createObjectURL(stream);
+  // Now hold of the element on the page that can contain the video
+  // (the video element with id 'webcam') and set its src attribute
+  // to the URL we created for the stream...
+  $('#webcam').attr({
+    src: streamURL
+  });
+  // And now that the webcam is available, we can start actually tracking
+  // colors with our tracking.js code...
+  startTracking();
+
+
+}
+
+// videoError (e)
 //
-// }
+// If there's a problem with getting the webcam, this will get called.
+// For now it just sets the background color to red to show something
+// went wrong. Not very sophisticated.
+function videoError(e) {
+  $('body').css({
+    'background-color': 'red'
+  })
+}
+
+// startTracking()
 //
-// // videoError (e)
-// //
-// // If there's a problem with getting the webcam, this will get called.
-// // For now it just sets the background color to red to show something
-// // went wrong. Not very sophisticated.
-// function videoError(e) {
-//   $('body').css({
-//     'background-color': 'red'
-//   })
-// }
+// Called when webcam is available. Sets up the face tracking.
+function startTracking() {
+
+  // Make a face tracker...
+  var faceTracker = new tracking.ObjectTracker(['face']);
+
+  // Set up the function to call each frame while tracking
+  faceTracker.on('track', handleTrackingEvent);
+
+  // Start tracking
+  tracking.track('#webcam', faceTracker);
+
+  // Also start an interval function that will check if the page
+  // should become sad
+  setInterval(checkSad,INTERVAL);
+
+
+}
+
+// updateFeelings ()
 //
-// // startTracking()
-// //
-// // Called when webcam is available. Sets up the face tracking.
-// function startTracking() {
+// Called every INTERVAL - updates how the page is feeling based on
+// when it last saw our face...
+function checkSad () {
+  // Update our variable tracking how long it's been since we saw a face...
+  timeSinceFace += 1;
+  // Note this is set back to 0 every frame that the tracker detects a face.
+
+  // Check whether it's been too long since we saw a face
+  // AND the happy face is still full opacity (i.e. not animating away)
+  if (timeSinceFace > MAX_TIME_SINCE_FACE && $('#yes').css('opacity') == 1) {
+    // If so, animate in the sad face and animate out the happy face
+    $('#no').animate({
+      opacity: 1
+    },ANIMATION_DURATION);
+    $('#yes').animate({
+      opacity: 0
+    },ANIMATION_DURATION);
+    // Say a random string from our sad lines
+    $('#no').text(getRandomString(noStrings));
+  }
+
+
+}
+
+// handleTrackingEvent
 //
-//   // Make a face tracker...
-//   var faceTracker = new tracking.ObjectTracker(['face']);
+// Called every frame of video that detection is running
+function handleTrackingEvent (event) {
+  // Check if anything was tracked (a face)
+  if (event.data.length === 0) {
+    // No faces were detected in this frame.
+  }
+  else {
+    // We found a face!
+    // Reset the time since we saw a face to 0
+    timeSinceFace = 0;
+    // If the happy face has opacity 0, we need to animate it in
+    // and animate out the sad face
+    if ($('#yes').css('opacity') == 0) {
+      $('#no').animate({
+        opacity: 0
+      },ANIMATION_DURATION);
+      $('#yes').animate({
+        opacity: 1,
+      },ANIMATION_DURATION);
+      // Speak a happy line
+      $('#yes').text(getRandomString(yesStrings));
+    }
+  }
+
+
+}
+
+
+
+
+
+
+
+// getRandomString (array)
 //
-//   // Set up the function to call each frame while tracking
-//   faceTracker.on('track', handleTrackingEvent);
-//
-//   // Start tracking
-//   tracking.track('#webcam', faceTracker);
-//
-//   // Also start an interval function that will check if the page
-//   // should become sad
-//   setInterval(checkSad,INTERVAL);
-//
-//
-// }
-//
-// // updateFeelings ()
-// //
-// // Called every INTERVAL - updates how the page is feeling based on
-// // when it last saw our face...
-// function checkSad () {
-//   // Update our variable tracking how long it's been since we saw a face...
-//   timeSinceFace += 100;
-//   // Note this is set back to 0 every frame that the tracker detects a face.
-//
-//   // Check whether it's been too long since we saw a face
-//   // AND the happy face is still full opacity (i.e. not animating away)
-//   if (timeSinceFace > MAX_TIME_SINCE_FACE && $('#yes').css('opacity') == 1) {
-//     // If so, animate in the sad face and animate out the happy face
-//     $('#no').animate({
-//       opacity: 1
-//     },ANIMATION_DURATION);
-//     $('#yes').animate({
-//       opacity: 0
-//     },ANIMATION_DURATION);
-//     // Say a random string from our sad lines
-//     $('#no').text(getRandomString(noStrings));
-//   }
-//
-//
-// }
-//
-// // handleTrackingEvent
-// //
-// // Called every frame of video that detection is running
-// function handleTrackingEvent (event) {
-//   // Check if anything was tracked (a face)
-//   if (event.data.length === 0) {
-//     // No faces were detected in this frame.
-//   }
-//   else {
-//     // We found a face!
-//     // Reset the time since we saw a face to 0
-//     timeSinceFace = 0;
-//     // If the happy face has opacity 0, we need to animate it in
-//     // and animate out the sad face
-//     if ($('#yes').css('opacity') == 0) {
-//       $('#no').animate({
-//         opacity: 0
-//       },ANIMATION_DURATION);
-//       $('#yes').animate({
-//         opacity: 1,
-//       },ANIMATION_DURATION);
-//       // Speak a happy line
-//       $('#yes').text(getRandomString(yesStrings));
-//     }
-//   }
-//
-//
-// }
-//
-//
-//
-//
-//
-//
-//
-// // getRandomString (array)
-// //
-// // A helper function that just returns a random string from the provided
-// // array. This is a classic way to select a random element from an array.
-// function getRandomString(array) {
-//   var randomIndex = Math.floor(Math.random() * array.length);
-//   return array[randomIndex];
-// }
+// A helper function that just returns a random string from the provided
+// array. This is a classic way to select a random element from an array.
+function getRandomString(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
 
 
 ///////////////////////////////////////////////////////////////////////////
